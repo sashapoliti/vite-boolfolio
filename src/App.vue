557c8325a5edfe8612132ee8e7caa748aceba9a0
sashapoliti <sas.politi@gmail.com>
2024-06-11 16:56:04 +0200

    add page project list, ul with all projects

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <template>
-  <!-- <ul>
-    <li v-for="project in projects" :="project.id">
-      {{ project.title }}
-    </li>
-  </ul> -->
-
   <HeaderComponent />
   <main class="container">
     <router-view></router-view>
   </main>
-
-  <!-- <button @click="getAllProject">avanti</button> -->
 </template>
 
 <script>
@@ -27,27 +19,10 @@ export default {
   data() {
     return {
       store,
-      projects: [],
-      /* nextPage: 0, */
     };
   },
-  methods: {
-    getAllProject() {
-      /* this.nextPage++; */
-      axios
-        .get(
-          this.store.apiBaseUrl +
-            "/projects" /* , {params: {page: this.nextPage}} */
-        )
-        .then((response) => {
-          this.projects = response.data.results /* .data */;
-          console.log(this.projects);
-        });
-    },
-  },
-  mounted() {
-    this.getAllProject();
-  },
+  methods: {},
+  mounted() {},
 };
 </script>
 
--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,15 +1,48 @@
 <template>
-    <div>
-        <h1>Project List</h1>
-    </div>
+  <section>
+    <h1>Project list</h1>
+  </section>
+
+  <section>
+    <ul>
+      <li v-for="project in projects" :="project.id">
+        {{ project.title }}
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-    export default {
-        name: "ProjectList"        
-    }
+import axios from "axios";
+import { store } from "../store";
+
+
+export default {
+  name: "ProjectList",
+  data() {
+    return {
+      store,
+      projects: [],
+    };
+  },
+  methods: {
+    getAllProject() {
+      /* this.nextPage++; */
+      axios
+        .get(
+          this.store.apiBaseUrl +
+            "/projects" /* , {params: {page: this.nextPage}} */
+        )
+        .then((response) => {
+          this.projects = response.data.results /* .data */;
+          console.log(this.projects);
+        });
+    },
+  },
+  mounted() {
+    this.getAllProject();
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
