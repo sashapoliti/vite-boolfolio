363dd5d3cf53addff7aff6bcbf5accc961305a9d
sashapoliti <sas.politi@gmail.com>
2024-06-11 18:36:18 +0200

    add card component and imported in project list

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -4,11 +4,11 @@
   </section>
 
   <section>
-    <ul>
-      <li v-for="project in projects" :="project.id">
-        {{ project.title }}
-      </li>
-    </ul>
+    <div class="row">
+        <div class="col-12 col-lg-6 col-xl-4 col-xxl" v-for="project in projects" :key="project.id">
+            <ProjectCard />
+        </div>
+    </div>
   </section>
 </template>
 
@@ -16,9 +16,13 @@
 import axios from "axios";
 import { store } from "../store";
 
+import ProjectCard from "../components/ProjectCard.vue";
 
 export default {
   name: "ProjectList",
+  components: {
+    ProjectCard
+  },
   data() {
     return {
       store,
