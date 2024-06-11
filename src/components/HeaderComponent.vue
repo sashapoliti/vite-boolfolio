0035db123c03055cc00b261dd1f5121f56f04c16
sashapoliti <sas.politi@gmail.com>
2024-06-11 18:25:23 +0200

    add link for backoffice

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,35 +1,40 @@
 <template>
   <header>
     <nav class="navbar">
-        <ul class="navbar-nav">
-            <li class="nav-item" v-for="menuItem in menuItems">
-                <router-link class="nav-link" :to="{ name: menuItem.routeName }">
-                    {{ menuItem.name }}
-                </router-link>
-            </li>
-        </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item" v-for="menuItem in menuItems">
+          <router-link class="nav-link" :to="{ name: menuItem.routeName }">
+            {{ menuItem.name }}
+          </router-link>
+        </li>
+      </ul>
+      <div class="nav-item ms-auto">
+        <a href="http://localhost:8000" class="nav-link">
+          <i class="fa-solid fa-user"></i> <span>Area Riservata</span>
+        </a>
+      </div>
     </nav>
   </header>
 </template>
 
 <script>
 export default {
-    name: "HeaderComponent",
-    data() {
-        return {
-            menuItems: [
-                {
-                    name: "Home",
-                    routeName: "home",
-                },
-                {
-                    name: "Projects",
-                    routeName: "projects",
-                }
-            ],
-        }
-    }
-}
+  name: "HeaderComponent",
+  data() {
+    return {
+      menuItems: [
+        {
+          name: "Home",
+          routeName: "home",
+        },
+        {
+          name: "Projects",
+          routeName: "projects",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
