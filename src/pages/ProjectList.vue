eb9b02c0d8f8504eb690438eaeb329ad630de7d2
sashapoliti <sas.politi@gmail.com>
2024-06-12 12:33:13 +0200

    add pagination in project list with params

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -5,10 +5,47 @@
 
   <section>
     <div class="row">
-        <div class="col-12 col-lg-6 col-xl-4 col-xxl" v-for="project in projects" :key="project.id">
-            <ProjectCard :object="project" />
-        </div>
+      <div
+        class="col-12 col-lg-6 col-xl-4 col-xxl"
+        v-for="project in projects"
+        :key="project.id"
+      >
+        <ProjectCard :object="project" />
+      </div>
     </div>
+    <!-- pagination -->
+    <nav>
+      <ul class="pagination">
+        <li class="page-item">
+          <a
+            class="page-link"
+            :class="{ currentPage == 1 ? 'disabled' : '' }"
+            href="#"
+            @click.prevent="setParams(currentPage - 1)"
+            >Previous</a
+          >
+        </li>
+        <li class="page-item" v-for="page in totalPage" :key="page">
+          <a
+            class="page-link"
+            :class="currentPage == page ? 'active' : ''"
+            href="#"
+            @click.prevent="setParams(page)"
+          >
+            {{ page }}
+          </a>
+        </li>
+        <li class="page-item">
+          <a
+            class="page-link"
+            :class="{ currentPage == totalPage ? 'disabled' : '' }"
+            href="#"
+            @click.prevent="setParams(currentPage + 1)"
+            >Next</a
+          >
+        </li>
+      </ul>
+    </nav>
   </section>
 </template>
 
@@ -21,27 +58,37 @@ import ProjectCard from "../components/ProjectCard.vue";
 export default {
   name: "ProjectList",
   components: {
-    ProjectCard
+    ProjectCard,
   },
   data() {
     return {
       store,
       projects: [],
+      currentPage: 0,
+      totalPage: 0,
+      params: null,
     };
   },
   methods: {
-    getAllProject() {
-      /* this.nextPage++; */
-      axios
-        .get(
-          this.store.apiBaseUrl +
-            "/projects" /* , {params: {page: this.nextPage}} */
-        )
-        .then((response) => {
-          this.projects = response.data.results /* .data */;
-          console.log(this.projects);
-        });
+    setParams(numpage) {
+      this.currentPage = numpage;
+      this.params = {
+        page: this.currentPage,
+      }
+      this.getAllProject();
     },
+    getAllProject() {
+      axios
+        .get(this.store.apiBaseUrl + "/projects", { params: this.params })
+        .then((response) => {
+          console.log(response.data);
+          this.projects = response.data.results.data;
+          this.currentPage = response.data.results.current_page;
+          this.totalPage = response.data.results.last_page;
+
+          this.params = null;
+        });
+    }
   },
   mounted() {
     this.getAllProject();
